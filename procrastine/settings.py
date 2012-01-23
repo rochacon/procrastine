da658dfd71f7cd813da15b87a5f9f4c9bfe17f07
Rodrigo Chacon <rochacon@gmail.com>
2012-01-23 03:52:22 -0200

    Optimizations and adjustments to the default settings

--- procrastine/settings.py
+++ procrastine/settings.py
@@ -47,18 +47,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(ROOT, 'media')
+MEDIA_ROOT = os.path.join(ROOT, '..', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/uploads/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(ROOT, 'webroot')
+STATIC_ROOT = os.path.join(ROOT, '..', 'webroot')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -87,9 +87,10 @@ SECRET_KEY = 'adfrpl_t7l)435ly95_-bxz1l9_+ya$o%lc!hw*co+2j#+el3!'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    ('django.template.loaders.cached.Loader',(
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
